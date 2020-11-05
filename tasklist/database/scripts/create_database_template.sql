0a12a7ae38331232dc129c0e978a7e1c37515016
guidiamond <guilherme.batista770@gmail.com>
2020-11-05 18:29:28 -0300

    create database template working without localhost

--- tasklist/database/scripts/create_database_template.sql
+++ tasklist/database/scripts/create_database_template.sql
@@ -4,14 +4,14 @@ CREATE DATABASE tasklist;
 DROP DATABASE IF EXISTS tasklist_test;
 CREATE DATABASE tasklist_test;
 
-DROP USER IF EXISTS tasklist_admin@localhost;
-CREATE USER tasklist_admin@localhost IDENTIFIED BY "$DB_TASKLIST_ADMIN_PW";
-GRANT ALL ON tasklist.* TO tasklist_admin@localhost;
-GRANT ALL ON tasklist_test.* TO tasklist_admin@localhost;
+DROP USER IF EXISTS tasklist_admin@'%';
+CREATE USER tasklist_admin@'%' IDENTIFIED BY "$DB_TASKLIST_ADMIN_PW";
+GRANT ALL ON tasklist.* TO tasklist_admin@'%';
+GRANT ALL ON tasklist_test.* TO tasklist_admin@'%';
 
-DROP USER IF EXISTS tasklist_app@localhost;
-CREATE USER tasklist_app@localhost IDENTIFIED BY "$DB_TASKLIST_APP_PW";
-GRANT SELECT, INSERT, UPDATE, DELETE ON tasklist.* TO tasklist_app@localhost;
-GRANT SELECT, INSERT, UPDATE, DELETE ON tasklist_test.* TO tasklist_app@localhost;
+DROP USER IF EXISTS tasklist_app@'%';
+CREATE USER tasklist_app@'%' IDENTIFIED BY "$DB_TASKLIST_APP_PW";
+GRANT SELECT, INSERT, UPDATE, DELETE ON tasklist.* TO tasklist_app@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON tasklist_test.* TO tasklist_app@'%';
 
 COMMIT
