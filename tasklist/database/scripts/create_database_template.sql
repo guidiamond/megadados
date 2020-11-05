134c4b287a937c0df90249141e598fa295fe472c
Thomas Queiroz <thomasqueirozb@gmail.com>
2020-11-05 17:26:37 -0300

    mysql connector does not connect

--- tasklist/database/scripts/create_database_template.sql
+++ tasklist/database/scripts/create_database_template.sql
@@ -6,11 +6,15 @@ CREATE DATABASE tasklist_test;
 
 DROP USER IF EXISTS tasklist_admin@localhost;
 CREATE USER tasklist_admin@localhost IDENTIFIED BY "$DB_TASKLIST_ADMIN_PW";
+GRANT ALL ON tasklist.* TO tasklist_admin@'%';
+GRANT ALL ON tasklist_test.* TO tasklist_admin@'%';
 GRANT ALL ON tasklist.* TO tasklist_admin@localhost;
 GRANT ALL ON tasklist_test.* TO tasklist_admin@localhost;
 
 DROP USER IF EXISTS tasklist_app@localhost;
 CREATE USER tasklist_app@localhost IDENTIFIED BY "$DB_TASKLIST_APP_PW";
+GRANT SELECT, INSERT, UPDATE, DELETE ON tasklist.* TO tasklist_app@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON tasklist_test.* TO tasklist_app@'%';
 GRANT SELECT, INSERT, UPDATE, DELETE ON tasklist.* TO tasklist_app@localhost;
 GRANT SELECT, INSERT, UPDATE, DELETE ON tasklist_test.* TO tasklist_app@localhost;
 
