4b5861ae3fd7e209e753f1a839f85ad9a95a48dd
guidiamond <guilherme.batista770@gmail.com>
2020-11-06 00:54:56 -0300

    migrations for test

--- tasklist/database/migrations/0001_create_db.sql
+++ tasklist/database/migrations/0001_create_db.sql
@@ -5,3 +5,11 @@ CREATE TABLE tasks (
     description NVARCHAR(1024),
     completed BOOLEAN
 );
+
+USE tasklist_test;
+DROP TABLE IF EXISTS tasks;
+CREATE TABLE tasks (
+    uuid BINARY(16) PRIMARY KEY,
+    description NVARCHAR(1024),
+    completed BOOLEAN
+);
